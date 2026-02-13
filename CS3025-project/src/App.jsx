97f245ec6dcccf514dbae0bc745b1156162f6dff
sierrahowe1 <sierra.howe@unb.ca>
2026-02-13 09:26:39 -0400

    test

--- CS3025-project/src/App.jsx
+++ CS3025-project/src/App.jsx
@@ -3,10 +3,12 @@ import { Mail, Lock, Eye, EyeOff, User } from 'lucide-react';
 import { toast, Toaster} from 'sonner';
 import Login from "./components/Login";
 import Homepage from "./components/Homepage";
+import Messages from "./components/Messages";
 
 export default function App() {
  const [isLoggedIn, setLoggedIn] = useState(false);
  const [credentials, setCredentials] = useState(null); 
+ const [currentPage, setCurrentPage] = useState('homepage');
 
 
   const handleLogin = (user) => {
@@ -16,9 +18,24 @@ export default function App() {
  const handleLogout = () => {
   setLoggedIn(false);
   setCredentials(null);
+  setCurrentPage("homepage");
   toast.info("Logged out successfully!", {
     description: "You have logged out of your account.",
   });
+
+  const handleNavigate = (page) => {
+    setCurrentPage(page);
+  }
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'messaging': 
+        return <Messages onNavigate={handleNavigate} onLogout={handleLogout}/>
+      case 'homepage':
+        default:
+          return <Homepage userName={userData?.name} onLogout={handleLogout} onNavigate={handleNavigate}/>
+    }
+  }
 
   
   
